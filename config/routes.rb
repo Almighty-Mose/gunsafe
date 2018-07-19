7e1b5e09a7df385d398b1e4d957a6383ea625ce2
Branden Mosley <branden.mosley1337@gmail.com>
2018-07-19 12:09:36 -0700

    configured initial routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :user
+resources :firearms # We will nest accessories under firearms.
+
+
+root 'welcome#home'
+
 end
