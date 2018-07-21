2dadf3692c03beba0e9060b4641898d3553b04b1
Branden Mosley <branden.mosley1337@gmail.com>
2018-07-21 16:30:55 -0700

    added User view files, built users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,18 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      session[:user_id] = @user.id
 
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def show
