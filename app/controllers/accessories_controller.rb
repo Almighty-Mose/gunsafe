297424e3455e27cf3721709fc80559051d1e8f79
Branden Mosley <branden.mosley1337@gmail.com>
2018-08-02 13:10:58 -0700

    Fleshed out Accessories controller, created accessories show page

--- app/controllers/accessories_controller.rb
+++ app/controllers/accessories_controller.rb
@@ -6,11 +6,16 @@ class AccessoriesController < ApplicationController
   end
 
   def new
-
+    @accessory = Accessory.new
   end
 
   def create
-
+    @accessory = current_user.accessories.build(accessory_params)
+    if @accessory.save
+      redirect_to firearms_path
+    else
+      render :new
+    end
   end
 
   def show
@@ -22,16 +27,24 @@ class AccessoriesController < ApplicationController
   end
 
   def update
+    @accessory.update(accessory_params)
 
+    redirect_to @accessory
   end
 
   def destroy
+    @accessory.delete
 
+    redirect_to firearms_path
   end
 
   private
 
   def set_accessory
     @accessory = Accessory.find(params[:id])
+  end
+
+  def accessory_params
+    params.require(:accessory).permit(:name, :category, :price, :purchase_date)
   end
 end
