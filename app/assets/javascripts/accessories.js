6f7e0e6838289bfa6ee4b19b5ca1d3439d4f3933
Branden Mosley <branden.mosley1337@gmail.com>
2018-12-05 12:01:25 -0700

    Complete the Accessories drawer. Also rename sidenav.css to drawer.css

--- app/assets/javascripts/accessories.js
+++ app/assets/javascripts/accessories.js
@@ -2,23 +2,10 @@ $(function() {
   $("#accessoryList").on("click", "a", function() {
     let id = $(this).data("id");
     $.get("/accessories/" + id + ".json", function(data) {
-      console.log(data);
+      $(".accessoryName").text(data.name)
+      $(".accessoryPrice").text(data.price)
+      $(".accessoryPurchase").text(data.purchase_date)
     });
     document.getElementById("accessoryDrawer").style.width = "500px";
   });
 });
-
-// function insertFirearm(id) {
-//   $.get("/firearms/" + id + ".json", function (data) {
-//     var firearm = new Firearm(data);
-//     //this will insert the data response into the firearmDrawer element of firearms/index.html
-//     $(".js-next").attr("data-id", firearm.id);
-//     $(".firearmName").text(firearm.name);
-//     $(".firearmCaliber").text(firearm.caliber);
-//     $(".firearmSerial").text(firearm.serial);
-//     $(".firearmPrice").text(firearm.price);
-//     $(".firearmPurchase").text(firearm.purchaseDate);
-//     let accessories = data["accessories"];
-//     insertAccessories(accessories);
-//   });
-// }
