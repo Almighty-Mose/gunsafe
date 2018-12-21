9616a2e7c8d9b4ec0d105605f0a715cf2935809a
Branden <branden.mosley1337@gmail.com>
2018-12-20 18:37:45 -0700

    Begin building JS for accessory creation via AJAX

--- app/assets/javascripts/accessories.js
+++ app/assets/javascripts/accessories.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).on('turbolinks:load', function() {
   $("#accessoryList").on("click", "a", function() {
     let id = $(this).data("id");
     $.get("/accessories/" + id + ".json", function(data) {
@@ -7,5 +7,17 @@ $(function() {
       $(".accessoryPurchase").text(data.purchase_date)
     });
     $("#accessoryDrawer").css('width', '500px');
+  });
+
+  $("#new_accessory").on("submit", function(e) {
+    e.preventDefault();
+    let values = $(this).serialize();
+    debugger
+    let posting = $.post('/accessories', values);
+    debugger
+    posting.done(function(data) {
+      debugger
+      console.log(data)
+    });
   });
 });
