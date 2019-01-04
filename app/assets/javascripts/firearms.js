8f8262e781d041af6cb467b7fc186b1a1e1b8bef
Branden <branden.mosley1337@gmail.com>
2019-01-04 09:13:06 -0700

    Change Firearm constructor in firearms.js to class constructor

--- app/assets/javascripts/firearms.js
+++ app/assets/javascripts/firearms.js
@@ -11,14 +11,16 @@ function closeAccessoryDrawer() {
 }
 
 // Firearm class constructor creates firearm objects for later consumption by jQuery
-function Firearm(data) {
-  this.name = `${data["make"]} ${data["model"]}`;
-  this.caliber = data["caliber"];
-  this.price = data["price"];
-  this.serial = data["serial_number"];
-  this.purchaseDate = data["purchase_date"];
-  this.id = data["id"];
-  this.category = data["category"]
+class Firearm {
+  constructor(data) {
+    this.name = `${data["make"]} ${data["model"]}`;
+    this.caliber = data["caliber"];
+    this.price = data["price"];
+    this.serial = data["serial_number"];
+    this.purchaseDate = data["purchase_date"];
+    this.id = data["id"];
+    this.category = data["category"]
+  }
 }
 
 Firearm.prototype.insertIntoDrawer = function() {
